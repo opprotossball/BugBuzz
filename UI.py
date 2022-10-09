c3ac8e21fe1a1161a751adea827ae8cbf0968c85
Jan Stachurski <01169628@pw.edu.pl>
2022-10-09 23:21:33 +0200

    Bugs assets added. drawBug method implemented.

--- UI.py
+++ UI.py
@@ -1,18 +1,24 @@
 import pygame
 import math
 from Plansza import Plansza
+from Pole import Pole
+from Robal import *
 
 
 class UI:
 
-    def __init__(self, windowWidth, windowHeight):
+    def __init__(self, windowWidth, windowHeight, tileRadius, margin):
         self.backgroundColor = (80, 80, 80)
         self.tileColor = (153, 153, 153)
         self.resourcesColor = (0, 160, 0)
         self.hatcheryColor = (150, 45, 45)
 
+        self.margin = margin
+        self.tileRadius = tileRadius
         self.width = windowWidth
         self.height = windowHeight
+        self.xCenter = int(windowWidth / 2)
+        self.yCenter = int(windowHeight / 2)
         self.running = True
         self.cos30 = math.cos(math.pi / 6)
         self.sin30 = math.sin(math.pi / 6)
@@ -20,6 +26,16 @@ class UI:
         self.tiltAngle = math.pi / 2
         pygame.display.set_caption('Robale')
         self.screen.fill(self.backgroundColor)
+
+        self.beetleWhite = pygame.image.load("Assets/Bugs/BeetleWhite.png")
+        self.beetleBlack = pygame.image.load("Assets/Bugs/BeetleBlack.png")
+        self.spiderWhite = pygame.image.load("Assets/Bugs/SpiderWhite.png")
+        self.spiderBlack = pygame.image.load("Assets/Bugs/SpiderBlack.png")
+        self.antWhite = pygame.image.load("Assets/Bugs/AntWhite.png")
+        self.antBlack = pygame.image.load("Assets/Bugs/AntBlack.png")
+        self.grasshooperWhite = pygame.image.load("Assets/Bugs/GrasshooperWhite.png")
+        self.grasshooperBlack = pygame.image.load("Assets/Bugs/GrasshooperBlack.png")
+
 
     def updateWindow(self):
         while self.running:
@@ -41,25 +57,41 @@ class UI:
         y = int(yCenter + (tileRadius + margin) * (self.sin30 * (pole.q + pole.s) - pole.r))
         return (x, y)
 
-    def drawBoard(self, plansza, xCenter, yCenter, tileRadius, marigin):
+    def drawBoard(self, plansza):
         for pole in plansza.iterList:
-            coordinates = self.transformToRealCoordinates(pole, xCenter, yCenter, tileRadius, marigin)
+            coordinates = self.transformToRealCoordinates(pole, self.xCenter, self.yCenter, self.tileRadius, self.margin)
             if pole.hatchery:
-                self.drawHex(coordinates[0], coordinates[1], tileRadius, self.hatcheryColor)
+                self.drawHex(coordinates[0], coordinates[1], self.tileRadius, self.hatcheryColor)
             elif pole.resources:
-                self.drawHex(coordinates[0], coordinates[1], tileRadius, self.resourcesColor)
+                self.drawHex(coordinates[0], coordinates[1], self.tileRadius, self.resourcesColor)
             else:
-                self.drawHex(coordinates[0], coordinates[1], tileRadius, self.tileColor)
+                self.drawHex(coordinates[0], coordinates[1], self.tileRadius, self.tileColor)
+
+    def drawBug(self, bug, tile):
+        if isinstance(bug, Konik):
+            self.screen.blit(self.grasshooperWhite, self.transformToRealCoordinates(tile, self.xCenter, self.yCenter, self.tileRadius, self.margin))
+        elif isinstance(bug, Mrowka):
+            self.screen.blit(self.antWhite, self.transformToRealCoordinates(tile, self.xCenter, self.yCenter, self.tileRadius, self.margin))
+        elif isinstance(bug, Pajak):
+            self.screen.blit(self.spiderWhite, self.transformToRealCoordinates(tile, self.xCenter, self.yCenter, self.tileRadius, self.margin))
+        elif isinstance(bug, Zuk):
+            self.screen.blit(self.beetleWhite, self.transformToRealCoordinates(tile, self.xCenter, self.yCenter, self.tileRadius, self.margin))
 
 
 if __name__ == '__main__':
     screenWidth = 1920
     screenHeight = 1080
     pygame.init()
-    ui = UI(screenWidth, screenHeight)
+    ui = UI(screenWidth, screenHeight, 60, 4.5)
     board = Plansza()
-    ui.drawBoard(board, screenWidth / 2, screenHeight / 2, 60, 4.5)
+    ui.drawBoard(board)
+    p = Pole(2, 2, -4, -1)
+    m = Mrowka(1)
+    ui.drawBug(m, p)
     ui.updateWindow()
+    pl = Plansza()
+    pl.getPlansza()
+
 
 
 
