7b8d1257266d410a03803bbbc027ba264f8a0adb
alicjaoczkowska <alicja.oczkowska.stud@gmail.com>
2022-10-12 01:23:38 +0200

    Completed setNumberOfMoves() method in Armia class

--- Armia.py
+++ Armia.py
@@ -1,5 +1,6 @@
 from Robal import *
-
+from Pole import *
+from collections import Counter
 
 class Armia:
 
@@ -9,9 +10,23 @@ class Armia:
         self.lastID = self.lastID + 1
         return self.lastID
 
-    def __init__(self, newBug):
+    def __init__(self, pole ):
         self.ID = self.getNewID()
         self.numberOfMoves = 0
-        self.bugList = [newBug]
+        self.bugList = []
+        if pole.bug is not None:
+            self.bugList.add(self, pole.bug)
+            pole.bug.armyID = self.ID
 
-    # def setNumberOfMoves(self):
+    def setNumberOfMoves(self):
+        bugMoves = Counter(self.bugList.move)
+        sorted(bugMoves, key=bugMoves.keys(), reverse=True)
+
+        bugCount = 0
+        for count, value in bugMoves.items():
+            bugCount += count
+            if bugCount > bugMoves.total()*0.5:
+                moveValue = value
+                break
+
+        self.numberOfMoves = moveValue
