16657f04d6a4b3c3c179f9d5b6ba7b30f272a5de
Stanleyowskiki <s.t.malinski@gmail.com>
2022-10-27 10:31:53 +0200

    Refactorization of InterfejsGracza class

--- FrontEnd/InterfejsGracza.py
+++ FrontEnd/InterfejsGracza.py
@@ -3,20 +3,20 @@ from FrontEnd.Interfejs import Interfejs
 
 class InterfejsGracza(Interfejs):
     def getAttack(self, possible_attacks):
-        choice = -1
-        while 0 > choice or choice >= len(possible_attacks):
-            choice = int(input("Możliwe ruchy:" + possible_attacks))
-        return possible_attacks[choice]
+        return self.getInput(possible_attacks)
 
     def getMove(self, possible_moves):
-        choice = -1
-        while 0 > choice or choice >= len(possible_moves):
-            choice = int(input("Możliwe ruchy:" + possible_moves))
-        return possible_moves[choice]
+        return self.getInput(possible_moves)
 
     def getHatch(self, possible_hatch):
-        choice = -1
-        while 0 > choice or choice >= len(possible_hatch):
-            choice = int(input("Możliwe ruchy:" + str(possible_hatch)))
+        return self.getInput(possible_hatch)
 
-        return possible_hatch[choice]
+    def getInput(self, possible):
+        choice = -1
+        while 0 > choice or choice >= len(possible):
+            chosen = input("\tMożliwe ruchy:" + str(possible) + " ")
+            if chosen == '':
+                choice = 0
+            else:
+                choice = int(chosen)
+        return possible[choice]
