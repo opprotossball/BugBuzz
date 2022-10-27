4646c8a21e440c77b734b56c1037972a0c342eec
Stanleyowskiki <s.t.malinski@gmail.com>
2022-10-27 11:06:25 +0200

    Army forming continuation

--- FrontEnd/GameMaster.py
+++ FrontEnd/GameMaster.py
@@ -1,9 +1,5 @@
 from BackEnd.Armia import Armia
 from BackEnd.Plansza import Plansza
-from BackEnd.Robal import Robal
-from InterfejsGracza import InterfejsGracza
-
-import time
 
 
 class GameMaster:
@@ -11,45 +7,57 @@ class GameMaster:
         self.plansza = Plansza(5)
         self.turn = 0
 
+        self.BlackPlayer = None
+        self.WhitePlayer = None
+
     def newGame(self, player_white, player_black):
         self.BlackPlayer = player_black
         self.WhitePlayer = player_white
-        while(True):
+        while True:
             print("Tura białego atak.")
             self.WhitePlayer.resources += self.getResourcesForSide("B")
             self.WhitePlayer.performAttack()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print("Tura białego ruch.")
             self.WhitePlayer.performMove()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print("Tura białego wylęganie.")
             self.WhitePlayer.performHatchery()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print("Tura czarnego atak.")
             self.BlackPlayer.resources += self.getResourcesForSide("C")
             self.BlackPlayer.performAttack()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print("Tura czarnego ruch.")
             self.BlackPlayer.performMove()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print("Tura czarnego wylęganie.")
             self.BlackPlayer.performHatchery()
-            time.sleep(0.5)
+            #time.sleep(0.5)
 
     def getArmies(self, side):
         armies = []
 
-        fields_of_plane = self.plansza.iterList
+        if side == "B":
+            player = self.WhitePlayer
+        elif side == "C":
+            player = self.BlackPlayer
+        else:
+            return
 
-        for pole in fields_of_plane:
-            if pole.bug is not None and pole.bug.side == side:
-                army = Armia()
-                army.addRobal(pole.bug)
-                for direction in pole.neighbours:
-                    if direction is not None and direction.bug.side == side:
-                        army.addRobal(direction.bug)
-                        fields_of_plane.remove(direction.bug)
-                armies.append(army)
+        for bug in player.bugList:
+            bug.army = None
+
+        for bug in player.bugList:
+            if bug.army is not None:
+                break
+            army = Armia()
+            army.addBug(bug)
+            bug.recruitNeighbours()
+            army.setMoves()
+            armies.append(army)
+
+        return armies
 
     def getResourcesForSide(self, side):
         resources = self.plansza.resources
@@ -59,7 +67,6 @@ class GameMaster:
             if field.bug is not None and field.bug.side == side:
                 n += field.bug.army.numberOfGrassHopppers
         return n
-
 
     def isAvailableSpaceForHatch(self, side):
         hatchery = []
@@ -73,7 +80,6 @@ class GameMaster:
                 return True
         return False
 
-
     def getAvialabelSpaceForHatch(self, side):
         if side == "B":
             hatchery = self.plansza.whitesHatchery
@@ -84,10 +90,6 @@ class GameMaster:
             if hatch.bug is not None:
                 option.append(hatch)
         return option
-
-    def addBug(self, selectedPlacmentField, bugID):
-        bug = Robal(bugID)
-        selectedPlacmentField.addBug(bug)
 
     def setUI(self, ui):
         self.ui = ui
