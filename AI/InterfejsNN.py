49290c5703b8d796f20b04e5a04962838c0735c6
Stanleyowskiki <s.t.malinski@gmail.com>
2022-11-02 13:45:43 +0100

    file InterfejsNN: added InterfejsNN: created interface for NN to interact with.

---
+++ AI/InterfejsNN.py
@@ -0,0 +1,46 @@
+from FrontEnd.Interfejs import Interfejs
+
+
+class InterfejNN(Interfejs):
+    def __init__(self, evaluator, GM, side, updateMethod):
+        super().__init__(GM, side, updateMethod)
+        self.side = side
+        self.evaluator = evaluator
+        self.moves = None # ([ruchy armii], [ataki armii(lista priorytetu ataku)], [wylęganie])
+
+    def yourTurn(self):
+        position = self.GM.board.clone()
+        self.moves = self.evaluator.evaluate_and_get_best_move(position)
+        self.moves_game = self.moves[0]
+        self.move_counter = 0
+        self.attack_game = self.moves[1]
+        self.attack_counter = 0
+        self.hatch_game = self.moves[2]
+        self.hatch_counter = 0
+
+    def getMove(self, possible_moves):
+        move = self.moves_game[self.move_counter]
+        self.move_counter += 1
+        if move not in possible_moves:
+            print("Invalid move:" + move + " not in " + possible_moves)
+            return None
+        else:
+            return move
+
+    def getAttack(self, possible_attacks):
+        attack = self.attack_game[self.attack_counter]
+        self.move_counter += 1
+        if attack not in possible_attacks:
+            print("Invalid move:" + attack + " not in " + possible_attacks)
+            return None
+        else:
+            return attack
+
+    def getHatch(self, possible_hatch):
+        hatch = self.hatch_game[self.hatch_game]
+        self.move_counter += 1
+        if hatch not in possible_hatch:
+            print("Invalid move:" + hatch + " not in " + possible_hatch)
+            return None
+        else:
+            return hatch
