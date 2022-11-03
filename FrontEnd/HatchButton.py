4f967e0d4baa156c2602d5db04214b86817b753c
Jan Stachurski <01169628@pw.edu.pl>
2022-11-03 17:10:45 +0100

    "HatchButton class added"

---
+++ FrontEnd/HatchButton.py
@@ -0,0 +1,37 @@
+import pygame
+
+class HatchButton:
+    clickedLeft = False
+
+    def __init__(self, image, imageSelected, bugShortName):
+        self.image = image
+        self.imageSelected = imageSelected
+        self.bugShortName = bugShortName
+        self.selected = False
+        self.rect = None
+
+    def draw(self, surface, x, y, scale):
+        if self.selected:
+            image = self.imageSelected
+        else:
+            image = self.image
+        image = pygame.transform.smoothscale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
+        self.rect = image.get_rect()
+        self.rect.topleft = (x, y)
+        surface.blit(image, (x, y))
+
+    def isClickedLeft(self):
+        if self.rect == None:
+            print("draw button first")
+            return False
+        action = False
+        mousePosition = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0] == 0:
+            HatchButton.clickedLeft = False
+        elif self.rect.collidepoint(mousePosition) and not HatchButton.clickedLeft:
+            self.selected = True
+            HatchButton.clickedLeft = True
+            action = True
+        elif not self.rect.collidepoint(mousePosition):
+            self.selected = False
+        return action
