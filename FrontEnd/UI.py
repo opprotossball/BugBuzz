55b4e095869e4818cd4576c6d89bcf4032034dfd
Jan Stachurski <01169628@pw.edu.pl>
2022-11-07 22:57:16 +0100

    "Added texts to be show in game"

--- FrontEnd/UI.py
+++ FrontEnd/UI.py
@@ -5,28 +5,47 @@ import pygame
 
 
 class UI:
-    def __init__(self, gameMaster):
+    def __init__(self, game_master):
         self.tileButtons = []
         self.hatchButtons = []
-        self.gameMaster = gameMaster
-        self.selectedTile = None
+        self.game_master = game_master
+        self.selected_tile = None
         self.chosenToHatch = None
         self.mode = None
         self.side = None
 
-        self.antHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/antHatchButton.png")
-        self.grasshooperHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/grasshooperHatchButton.png")
-        self.spiderHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/spiderHatchButton.png")
-        self.beetleHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/beetleHatchButton.png")
-        self.antHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/antHatchButtonSelected.png")
-        self.grasshooperHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/grasshooperHatchButtonSelected.png")
-        self.spiderHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/spiderHatchButtonSelected.png")
-        self.beetleHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/beetleHatchButtonSelected.png")
+        self.antHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/antWhiteHatchButton.png")
+        self.grasshooperHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/grasshooperWhiteHatchButton.png")
+        self.spiderHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/spiderWhiteHatchButton.png")
+        self.beetleHatchButton = pygame.image.load("./FrontEnd/Assets/Buttons/beetleWhiteHatchButton.png")
+
+        self.antHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/antWhiteSelectedHatchButton.png")
+        self.grasshooperHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/grasshooperWhiteSelectedHatchButton.png")
+        self.spiderHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/spiderWhiteSelectedHatchButton.png")
+        self.beetleHatchButtonSelected = pygame.image.load("./FrontEnd/Assets/Buttons/beetleWhiteSelectedHatchButton.png")
 
         self.hatchButtons.append(HatchButton(self.antHatchButton, self.antHatchButtonSelected, "M"))
         self.hatchButtons.append(HatchButton(self.grasshooperHatchButton, self.grasshooperHatchButtonSelected, "K"))
         self.hatchButtons.append(HatchButton(self.spiderHatchButton, self.spiderHatchButtonSelected, "P"))
         self.hatchButtons.append(HatchButton(self.beetleHatchButton, self.beetleHatchButtonSelected, "Z"))
+
+        self.phase_titles = [
+            "White's combat phase",
+            "White's move phase",
+            "White's hatch phase",
+            "Black's combat phase",
+            "Black's move phase",
+            "Black's hatch phase"
+        ]
+
+        self.tip_texts = [
+            "Select opponent's army to attack"
+            "Select and move your army"
+            "Hatch bug in your hatchery"
+            "Wait for opponent to play"
+            "Wait for opponent to play"
+            "Wait for opponent to play"
+        ]
 
     def setMode(self, mode, side):
         self.mode = mode
@@ -43,34 +62,6 @@ class UI:
                 tiles.append(anotherBug.field)
         return tiles
 
-    # Useless test function
-    def drawRandomBug(self, tile):
-        if tile.bug is not None:
-            return
-        x = randrange(8)
-        bug = None
-        if x == 0:
-            bug = Zuk("C")
-        elif x == 1:
-            bug = Zuk("B")
-        elif x == 2:
-            bug = Pajak("C")
-        elif x == 3:
-            bug = Pajak("B")
-        elif x == 4:
-            bug = Mrowka("C")
-        elif x == 5:
-            bug = Mrowka("B")
-        elif x == 6:
-            bug = Konik("C")
-        elif x == 7:
-            bug = Konik("B")
-        bug.moveBugTo(tile)
-        if bug.side == "B":
-            self.gameMaster.WhitePlayer.bugList.append(bug)
-        elif bug.side == "C":
-            self.gameMaster.BlackPlayer.bugList.append(bug)
-
     def getInput(self):
         if self.mode == "Move":
             for tileButton in self.tileButtons:
@@ -78,27 +69,27 @@ class UI:
                     tile = tileButton.tile
                     if tile.bug is None:
                         if not self.makeMove(tile):
-                            self.selectedTile = None
+                            self.selected_tile = None
                     else:
-                        self.selectedTile = tile
-                    selectedArmyTiles = self.selectArmy(tile)
-                    self.gameMaster.display.highlightedTiles = selectedArmyTiles
+                        self.selected_tile = tile
+                    selected_army_tiles = self.selectArmy(tile)
+                    self.game_master.display.highlightedTiles = selected_army_tiles
 
         for hatchButton in self.hatchButtons:
             if hatchButton.isClickedLeft():
                 self.chosenToHatch = hatchButton.bugShortName
 
     def makeMove(self, tile):
-        if self.selectedTile is None:
+        if self.selected_tile is None:
             return False
-        dictionary = self.selectedTile.getDictionary()
+        dictionary = self.selected_tile.getDictionary()
         directions = [d for d, n in dictionary.items() if n == tile]
         if directions.__len__() == 0:
             return False
         direction = directions[0]
-        leader = self.selectedTile.bug
+        leader = self.selected_tile.bug
         leader.army.performMove(direction)
-        self.selectedTile = leader.field
-        self.gameMaster.getArmies(leader.side)
-        self.gameMaster.display.highlightedTiles = self.selectArmy(tile)
+        self.selected_tile = leader.field
+        self.game_master.getArmies(leader.side)
+        self.game_master.display.highlightedTiles = self.selectArmy(tile)
         return True
