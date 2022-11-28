12b009bf9334b16c46ad867c6c6a86c2bf7b69da
Jan Stachurski <01169628@pw.edu.pl>
2022-11-28 14:57:31 +0100

    "Robal.py : clone_with_field() method added Pole.py : get_key_for() method added GameMechanic.py : methods for setting positions added GeneratorPlayer.py : clone() split into clone_for_hatch() and clone_for_move() Armia.py : removed unused methods PositionGenerator.py : get_moves() method implemented "

--- BackEnd/GameMechanic/GeneratorPlayer.py
+++ BackEnd/GameMechanic/GeneratorPlayer.py
@@ -10,9 +10,34 @@ class GeneratorPlayer(Player):
     def set_state(self, state):
         pass
 
-    def clone(self):
+    def clone_for_hatch(self):
         new = GeneratorPlayer(self.gm, self.side)
         new.bugs_available = copy.deepcopy(self.bugs_available)
-        # new.bugList = copy.deepcopy(self.bugList) # niepotrzebne na razie
         new.resources = self.resources
         return new
+
+    def clone_for_move(self):
+        new = GeneratorPlayer(self.gm, self.side)
+        for bug in self.bugList:
+            new.bugList.append(bug.clone_with_field())
+        return new
+
+    # def get_hash_for_move(self):
+    #     code = ""
+    #     tiles = [[] for _ in range(4)]
+    #     for bug in self.bugList:
+    #         if bug.short_name == 'K':
+    #             i = 0
+    #         elif bug.short_name == 'M':
+    #             i = 1
+    #         elif bug.short_name == 'P':
+    #             i = 2
+    #         else:
+    #             i = 3
+    #         tiles[i].append(bug.field.get_key_for())
+    #     for type_list in tiles:
+    #         type_list.sort()
+    #     for type_list in tiles:
+    #         for c in type_list:
+    #             code += str(c)
+    #     return code
