32e4e6c6afce60a47f8108fc9b7a3cef24b0dd31
Stanleyowskiki <s.t.malinski@gmail.com>
2022-12-03 20:25:17 +0100

    file DirectionEnum: created DirectionEnum: Enumarating class for directions. Robal: Added Enumarating class for states of move. Armia: Altered for new enums. Plansza: Altered for new enums.

--- Util/Information.py
+++ Util/Information.py
@@ -1,4 +1,6 @@
+from Util.DirectionEnum import Direction
 
-directionOptions = ["WN", "W", "WS", "ES", "E", "EN"]
+directionOptions = [Direction.WN, Direction.W, Direction.WS, Direction.ES, Direction.E, Direction.EN]
 resourceFieldCoordinates = [[1, 0, -1], [-2, 3, -1], [1, -3, 2]]
 board_size = 4
+
